f3766b9323fcf48f0d4ddbd471c211c767574380
Thafsi PV <pvthafsi@gmail.com>
2023-05-12 19:36:21 +0530

    Error changes

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,13 +1,32 @@
-import { useNavigate } from 'react-router-dom';
-import './Error.css'
+import { Link, useNavigate } from "react-router-dom";
+import "./Error.css";
+import Layout1 from "../../components/layout/Layout1";
+import { TbError404 } from "react-icons/tb";
 
 const Error = () => {
-    const navigate=useNavigate()
   return (
-    <div className="error-container">
-      Error page
-      <p onClick={()=>navigate("/home")}>goto home</p>
-    </div>
+    <>
+      <Layout1>
+        <div className="login-container">
+          <div className="login-input-container">
+            <TbError404 className="notfound-icon" />
+            <div className="button-div"></div>
+          </div>
+          <div className="error-title">
+            <p>Page Not found</p>
+          </div>
+          <div className="sign-upnav">
+            <div>
+              <p>
+                <Link to="/home">
+                  <span>Back to home</span>
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </Layout1>
+    </>
   );
 };
 
